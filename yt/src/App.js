40853114ab38e37ce4492cc6b1da7f5c02e7ff5e
DhananjaySrinivas <Dhanu9gowda@gmail.com>
2023-04-12 13:23:43 +0530

    Adding menu Toggler

--- yt/src/App.js
+++ yt/src/App.js
@@ -1,11 +1,15 @@
 import Body from "./components/Body";
 import Head from "./components/Head";
+import { Provider } from "react-redux";
+import store from "./utils/store"
 
 export default function App() {
   return (
+    <Provider store={store}>
     <div>
       <Head/>
       <Body/>
       </div>
+      </Provider>
   )
 }
