a453891a5667ce2cec77cff039d4ab61a3f24e34
DhananjaySrinivas <Dhanu9gowda@gmail.com>
2023-04-12 13:43:04 +0530

    Adding Buttons on the top of the page

---
+++ yt/src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({name}) => {
+  return (
+    <button className='px-5 m-2 py-1 bg-gray-300 rounded-md'>{name}</button>
+  )
+}
+
+export default Button
--- yt/src/components/ButtonList.js
+++ yt/src/components/ButtonList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Button from './Button'
 
 const ButtonList = () => {
+
+  const List = ["All","Virat Kohli","Gaming","songs","Live","Soccer","Cricket","Cooking","Valentines","Football"]
   return (
-    <div>
-      
+    <div className='flex'>
+      {List.map(list=>{
+        return <Button name={list}/>
+      })}
+     
     </div>
   )
 }
