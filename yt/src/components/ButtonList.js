eb52a0387a4bebed89009ab48d5c323e762ad8cc
DhananjaySrinivas <Dhanu9gowda@gmail.com>
2023-04-12 16:00:06 +0530

    Adding Video Card

--- yt/src/components/ButtonList.js
+++ yt/src/components/ButtonList.js
@@ -6,8 +6,8 @@ const ButtonList = () => {
   const List = ["All","Virat Kohli","Gaming","songs","Live","Soccer","Cricket","Cooking","Valentines","Football"]
   return (
     <div className='flex'>
-      {List.map(list=>{
-        return <Button name={list}/>
+      {List.map((list,index)=>{
+        return <Button key={index} name={list}/>
       })}
      
     </div>
--- yt/src/components/VideoContainer.js
+++ yt/src/components/VideoContainer.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { YOUTUBE_VIDEOS_API } from "./../utils/Constant"
+import VideoCard  from './VideoCard';
 
 const VideoContainer = () => {
+const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    getMovies();
+  },[])
+
+  const getMovies= async()=>{
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    setVideos(json.items)
+  }
+
   return (
-    <div>VideoCotainer</div>
+    
+    <div className='flex flex-wrap'>
+      {videos.map(videos=>{
+       return <VideoCard key={videos.id} info={videos}/>
+      })}
+      
+      </div>
   )
 }
 
