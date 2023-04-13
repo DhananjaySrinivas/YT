a545d868fdc651537dd9a491fd4e7db9fbd06c37
DhananjaySrinivas <Dhanu9gowda@gmail.com>
2023-04-13 22:06:34 +0530

    Add Comments Section

--- yt/src/components/WatchPage.js
+++ yt/src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { CloseMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom';
 import VideoContainer from './VideoContainer';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
     const[VideosSearch] = useSearchParams()
@@ -13,12 +14,17 @@ const WatchPage = () => {
     },[])
   return (
     <div className='px-5'>
+
+ 
+    <div >
         <iframe
          width="1200" height="600" 
          src={"https://www.youtube.com/embed/"+VideosSearch.get("v")}
          title="YouTube video player" 
          frameBorder="0" 
          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+    <CommentsContainer/>
     </div>
   )
 }
